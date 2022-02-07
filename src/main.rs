ff359dc327cb7ceed3e277c0e6a5724c5fd938e3
satbh <carlosarcia1827@gmail.com>
2022-02-07 15:16:38 -0600

    bugfix: removes windows from stack, and resizes whenever a window is destroyed

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use xcb::base::Connection;
-use xcb::{MAP_REQUEST, CW_EVENT_MASK, EVENT_MASK_SUBSTRUCTURE_REDIRECT, EVENT_MASK_SUBSTRUCTURE_NOTIFY, Window, GenericEvent, CONFIG_WINDOW_WIDTH};
+
+use xcb::{
+    MAP_REQUEST, CW_EVENT_MASK, EVENT_MASK_SUBSTRUCTURE_REDIRECT, EVENT_MASK_SUBSTRUCTURE_NOTIFY, Window, GenericEvent
+};
+
 use xcb;
 use std::process::Command;
 fn main() {
-    let (connection, _) = Connection::connect(Some(":0")).unwrap();
+    let (connection, _) = Connection::connect(Some(":2")).unwrap();
     let screen = connection.get_setup().roots().nth(0).unwrap();
     let mut windows: Vec<Window> = Vec::new();
     let root: Window = screen.root();
-    set_wallpaper();
+    //set_wallpaper();
     xcb::change_window_attributes_checked(
         &connection,
          root,
@@ -20,10 +24,26 @@ fn main() {
                 };
                 windows.push(casted_event.window());
                 xcb::map_window(&connection, casted_event.window());
-                //xcb::set_input_focus(&connection, XCB_INPUT_FOCUS_POINTER_ROOT as u8, casted_event.window(), 0);
-               // xcb::configure_window(&connection, casted_event.window(), &values);
-                manage_windows(&connection, (screen.width_in_pixels() as u32, screen.height_in_pixels() as u32), &windows);
+                manage_windows(
+                    &connection,
+                    (screen.width_in_pixels() as u32, screen.height_in_pixels() as u32),
+                    &windows
+                );
 
+                connection.flush();
+            },
+            xcb::DESTROY_NOTIFY => {
+                let casted_event: &xcb::DestroyNotifyEvent= unsafe {
+                    xcb::cast_event(event as &GenericEvent)
+                };
+                if let Some(index) = windows.iter().position(|&x| x == casted_event.window()) {
+                    windows.remove(index);
+                }
+                manage_windows(
+                    &connection,
+                    (screen.width_in_pixels() as u32, screen.height_in_pixels() as u32),
+                    &windows
+                );
                 connection.flush();
             },
             _ => {}
@@ -33,7 +53,7 @@ fn main() {
 
 fn set_wallpaper() {
     Command::new("sh")
-        .args(["-c", "DISPLAY=:0 hsetroot -cover /home/satbh/wallpapers/everforest"])
+        .args(["-c", "DISPLAY=:2 hsetroot -cover /home/satbh/wallpapers/everforest"])
         .output()
         .unwrap();
 }
